0671efb04015ff54ae1d90be4cd24b7c747a0f90
Carl Baldwin <carl@ecbaldwin.net>
2021-05-15 09:49:45 -0600

    add IPv4 implementation that returns a 4-byte address (#21)
    * add IPv4 implementation that returns a 4-byte address
    
    * add IPv4Net returning 4-byte IPNet with given prefix len

--- net_utils_test.go
+++ net_utils_test.go
@@ -260,3 +260,12 @@ func TestIPLessThan(t *testing.T) {
 		"2001:43::"+
 		"]", fmt.Sprintf("%s", ips))
 }
+
+func TestIPv4(t *testing.T) {
+	assert.Equal(t, ParseIP("127.0.0.1"), IPv4(127, 0, 0, 1))
+}
+
+func TestIPv4Net(t *testing.T) {
+	lo, _ := ParseCIDRToNet("127.0.0.1/8")
+	assert.Equal(t, *lo, IPv4Net(127, 0, 0, 1, 8))
+}
